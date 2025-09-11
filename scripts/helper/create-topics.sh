1b97e8dbdc667a3477487948d17e199b84be1f2e
Alexander <admin@inkatlas.com>
2025-09-11 11:55:53 +0300

    adding INIT_TOPICS env var to create topics on start

--- scripts/helper/create-topics.sh
+++ scripts/helper/create-topics.sh
@@ -10,3 +10,7 @@ KAFKA_CLUSTER_ID=$(get_kafka_cluster_id_from_container)
 auth="superUser:${SUPER_USER_PASSWORD}"
 
 create_topic kafka1:8091 ${KAFKA_CLUSTER_ID} users true ${auth}
+
+for TOPIC in ${INIT_TOPICS}; do
+	create_topic kafka1:8091 ${KAFKA_CLUSTER_ID} ${TOPIC} true ${auth}
+done
